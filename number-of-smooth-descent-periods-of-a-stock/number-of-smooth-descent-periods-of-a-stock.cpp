9341a3f004d0af391650f9dde053f13269a3220c
Borugadda Manoj <borugadda.manoj@gmail.com>
2021-12-29 15:13:58 +0530

    Time: 186 ms (23.76%), Space: 93.3 MB (37.20%) - LeetHub

---
+++ number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices)
+    {
+        int start=0;
+        int end=0;
+        long ans = 1;
+        for(end=1;end<prices.size();end++)
+        {
+            if(prices[end] == prices[end-1] - 1)
+            {
+                ans += end-start+1;
+            }
+            else
+            {
+                start = end;
+                ans += end-start+1;
+
+            }
+        }
+        return ans;
+    }
+};
