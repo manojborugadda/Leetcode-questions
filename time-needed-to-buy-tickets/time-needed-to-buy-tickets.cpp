9af1b62346dc69f4b0ae158eae49053cf6b72c40
Borugadda Manoj <borugadda.manoj@gmail.com>
2021-12-31 01:21:59 +0530

    Update time-needed-to-buy-tickets.cpp

--- time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) 
     {
-            int ans =0;
-            int n = tickets.size();
-            int ele = tickets[k];
-            for(int i=0;i< n; i++){
-                    if(i<=k){
-                        ans+= min(ele, tickets[i]);
-                    }else{
-                        ans+= min(ele-1, tickets[i]);   
-                    }
+            
+        int count = 0;
+        while(tickets[k]!=0){
+            for(int i = 0;i<tickets.size();i++){
+                if(tickets[i]>0){
+                    tickets[i]--;
+                    count++;
+                }
+                
+                if(tickets[k]==0){
+                    break;
+                }
+                    
+                }
             }
-            return ans;
+        return count;
     }
 };
