10d54cf0c7ec288c959de08405396e3788bb848d
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-01-03 22:06:27 +0530

    Time: 4 ms (39.58%), Space: 5.9 MB (84.60%) - LeetHub

---
+++ happy-number/happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isHappy(int n)
+    {
+        int sum = 0;
+
+        while (n > 0)
+        {
+            sum += pow(n % 10, 2);
+            n /= 10;
+        }
+
+        if (sum == 4)
+        {
+            return 0;
+        }
+
+        if (sum != 1)
+        {
+            return isHappy(sum);
+        }
+
+        return sum;
+    }
+};
