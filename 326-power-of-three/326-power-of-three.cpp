4e9b171f1e8776cc7cb429046f15483ee50d68e4
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-01-07 02:31:32 +0530

    Time: 51 ms (5.22%), Space: 5.8 MB (72.26%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    bool isPowerOfThree(long long n)
-    {   
-       if (n <= 0)
-        return false;
-    if (n % 3 == 0)
-        return isPowerOfThree(n / 3);
-    if (n == 1)
-        return true;
-    return false;
-    }
+    // bool isPowerOfThree(long long n)
+    // {   
+    //  if (n <= 0)
+    //     return false;
+    // if (n % 3 == 0)
+    //     return isPowerOfThree(n / 3);
+    // if (n == 1)
+    //     return true;
+    // return false;
+    // }
     
+    bool isPowerOfThree(int n) {
+if(n<1)
+return false;
+if(n%3!=0 && n!=1)
+return false;
+if(n==1)
+return true;
+return isPowerOfThree(n/3);
+    }
 };
