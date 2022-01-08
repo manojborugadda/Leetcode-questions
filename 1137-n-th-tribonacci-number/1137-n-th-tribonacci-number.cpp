46387100b9f377216087bacb4d0ef3f35d4f8634
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-01-08 22:26:30 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (49.91%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int tribonacci(int n)
+    {
+    if(n == 0 || n == 1){  // DP using tabulation method...........
+        return n;
+    }
+        
+    int arr[n+1];
+    arr[0] = 0;
+    arr[1] = 1;
+    arr[2] = 1;
+        
+    for(int i=3;i<=n;i++){
+        arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+    }
+        
+    return arr[n];
+        
+    }
+};
