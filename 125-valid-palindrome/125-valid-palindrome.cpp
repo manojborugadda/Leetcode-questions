20b5eff4d626f38df79b804fbe4b7985f47dc569
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-01-13 23:58:03 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (49.49%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) 
+    { int l=0,r=s.length()-1;
+        while(l<r){
+            if (!isalnum(s[l]) || s[l] == ' ')
+                l++;
+            else if(!isalnum(s[r]) || s[r] == ' ')
+                r--;
+            else{
+                if(tolower(s[l])!=tolower(s[r]))
+                    return false;
+                l++;
+                r--;
+            }//end else   
+        }//end while
+        return true;
+    }
+};
