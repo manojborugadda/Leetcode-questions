be9b222868f63117d3a55858ddc68473567f8e78
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-01-22 23:56:21 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (72.06%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        nums1.erase(nums1.begin()+m,nums1.end());
+        for(int i=0;i<nums2.size();i++){
+            nums1.push_back(nums2[i]);
+        }
+        
+        sort(nums1.begin(),nums1.end());
+    }
+};
