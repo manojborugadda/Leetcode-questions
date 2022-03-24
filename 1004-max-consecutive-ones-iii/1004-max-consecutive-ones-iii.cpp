1c491f5d75c6d8a05ec45b206fe25aeb18e16ecd
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-03-24 23:57:09 +0530

    Time: 85 ms (52.90%), Space: 55.4 MB (64.98%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -0,0 +1,15 @@
+class Solution { // sliding window and Two Pointer approach
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i = 0, ans = 0, zero_count = 0;
+        for(int j = 0;j<nums.size();j++){
+            if(nums[j]==0) zero_count++;
+            while(zero_count > k){ // if "zero count" is greater than "k" , we  move "i" while checking if nums[i] is 0 are not and decreasing the zero count untill it equals K
+                if(nums[i]==0) zero_count--;
+                i++;
+            }
+            ans = max(ans,j-i+1); // taking the length of the the maximum number of consecutive 1's in the array
+        }
+        return ans;
+    }
+};
