453d7bb38bca2a6a54d57921c88df923fb5bfeff
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-03-26 23:52:38 +0530

    Time: 57 ms (38.64%), Space: 27.5 MB (63.85%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0 , high = nums.size()-1;
+        while(low<=high){
+            int mid = (high+low)>>1;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if( target < nums[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+};
