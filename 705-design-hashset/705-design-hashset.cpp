d84424a338ce6332b94085965d3c6e29a9580847
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-04-21 17:56:37 +0530

    Time: 137 ms (55.91%), Space: 43.7 MB (40.31%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.cpp
@@ -0,0 +1,30 @@
+class MyHashSet { 
+public:
+                    // Time: O(1)
+                    // Space: O(n)
+
+    vector<bool>table;
+    MyHashSet() {
+       table.resize(1e6+1,false);
+    }
+    
+    void add(int key) {
+        table[key] = true;
+    }
+    
+    void remove(int key) {
+         table[key] = false;
+    }
+    
+    bool contains(int key) {
+        return table[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
