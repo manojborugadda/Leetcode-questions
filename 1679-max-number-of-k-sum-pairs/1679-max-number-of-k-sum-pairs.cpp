b94a2f065518232cbfd49d0c57d1ab6c78339bad
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-05-04 09:57:00 +0530

    Time: 270 ms (19.93%), Space: 58.2 MB (80.67%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,20 +1,42 @@
+// class Solution {
+// public:
+//     int maxOperations(vector<int>& nums, int k) {
+//         unordered_map<int,int>mp;
+//         int count=0;
+//     for(auto x:nums)
+//     {
+//         if(mp[k-x]) // if it is present in the MAP
+//         {
+//             //// removing the pairs from the MAP
+//             mp[(k-x)]--; 
+//             mp[x]--;
+            
+//             count++;
+//         }
+//         mp[x]++;
+//     }
+//         return count;
+//     }
+// };
+
+//using the TWO POINTER approach
+
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        int count=0;
-    for(auto x:nums)
-    {
-        if(mp[k-x]) // if it is present in the MAP
-        {
-            //// removing the pairs from the MAP
-            mp[(k-x)]--; 
-            mp[x]--;
-            
-            count++;
+       int i = 0, j = nums.size()-1;
+        int count = 0;
+        sort(nums.begin(),nums.end());  // sorting the array 
+        while(i<j){
+            if(nums[i]+nums[j]==k){
+                count++;
+                i++;j--;
+            }else if(nums[i]+nums[j] < k){
+                i++;
+            }else{
+                j--;
+            }
         }
-        mp[x]++;
-    }
         return count;
     }
 };
