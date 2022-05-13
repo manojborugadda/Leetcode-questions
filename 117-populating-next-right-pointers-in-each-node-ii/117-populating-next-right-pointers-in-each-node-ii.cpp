46857f70c926928a55765dc5e413f71782840cc1
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-05-13 20:24:41 +0530

    Time: 18 ms (54.04%), Space: 17.4 MB (62.31%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -16,38 +16,81 @@ public:
 };
 */
 
-class Solution { // this is using QUEUES,BFS 
-                 // TC:O(N) SC:O(N)
+// class Solution { // this is using QUEUES,BFS 
+//                  // TC:O(N) SC:O(N)
+// public:
+//     Node* connect(Node* root) {
+//         if(root==NULL) return NULL;
+//         queue<Node*>q;
+//         q.push(root);
+        
+//         while(q.empty() == false){
+            
+//             int n = q.size();
+            
+//             Node* dummy = NULL;  //  Next_Right_ELEMENT
+            
+//             for(int i = 0;i<n;i++){
+                
+//                auto top = q.front();
+//                 q.pop();
+//                 top->next = dummy;
+//                 dummy = top;
+//                 // in this problem we have to PUSH first Right element and Left element
+//                 if(top->right){
+//                     q.push(top->right);
+//                 }
+//                 if(top->left){
+//                     q.push(top->left);
+//                 }
+                
+//             }
+//         }
+        
+//         return root;
+        
+//     }
+// };
+
+
+
+
+///////////// Using Constant Space and O(N) time complexity
+
+class Solution { 
+                                    // TC:O(N) SC:O(1)
 public:
     Node* connect(Node* root) {
-        if(root==NULL) return NULL;
-        queue<Node*>q;
-        q.push(root);
+        Node* ans = root;
+            while(root != NULL)
+            {
+                Node *temp = new Node(-1);
+                Node* curr = temp;
+                
+                while(root!=NULL){
+                    
+                 if(root->left!=NULL){
+                     curr->next = root->left;
+                     curr = curr->next;
+                    }
+                    
+                    if(root->right!=NULL){
+                        curr->next = root->right;
+                        curr = curr->next;
+                    }
+                    
+                    root = root->next;
+                }
+                
+                root = temp->next;
+            }    
+        return ans;
         
-        while(q.empty() == false){
-            
-            int n = q.size();
-            
-            Node* dummy = NULL;  //  Next_Right_ELEMENT
-            
-            for(int i = 0;i<n;i++){
-                
-               auto top = q.front();
-                q.pop();
-                top->next = dummy;
-                dummy = top;
-                // in this problem we have to PUSH first Right element and Left element
-                if(top->right){
-                    q.push(top->right);
-                }
-                if(top->left){
-                    q.push(top->left);
-                }
-                
-            }
         }
-        
-        return root;
-        
-    }
 };
+
+
+
+
+
+
