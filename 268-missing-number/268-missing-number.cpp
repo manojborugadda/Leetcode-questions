a066138e5e505410508f0f90003a2cf57d2b1804
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-05-28 16:43:57 +0530

    Time: 31 ms (36.74%), Space: 17.9 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,17 +1,34 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int,int>mp;
-        for(auto x: nums){
-            mp[x]++;
-        }
+// class Solution {  // TC:O(N) SC:O(N) because we are using hashmap
+// public:
+//         int missingNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int,int>mp;
+//         for(auto x: nums){
+//             mp[x]++;
+//         }
         
-        for(int i = 0;i<=n;i++){
-            if(mp[i]==0){
-                return i;
+//         for(int i = 0;i<=n;i++){
+//             if(mp[i]==0){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
+
+ class Solution {  // TC:O(N) SC:O(1)
+ public:    
+        int missingNumber(vector<int>& nums) {
+            int ans,sum = 0 ;
+            int n = nums.size();
+            int total = n*(n+1)/2;  // sum of all N natural numbers
+            for(auto x:nums){
+                sum = sum+x; // sum of all elements in the array
             }
+            
+            ans = total-sum; // here we get the missing element by subtracting 
+            return ans;
         }
-        return -1;
-    }
-};
+    };
