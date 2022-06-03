cb324d43408de7fbf91c804bcc7739f6c240c2f3
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-06-04 00:13:16 +0530

    Time: 580 ms (56.73%), Space: 148.1 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,21 +1,27 @@
 class NumMatrix {
 public:   /// O(n*m)
-    int row, col;
-    vector<vector<int>> sum;
-    NumMatrix(vector<vector<int>>& matrix) {
-        row = matrix.size();
-        col = matrix[0].size();
-        sum = vector<vector<int>>(row+1, vector<int>(col+1, 0));
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                sum[i+1][j+1] = sum[i][j+1] + sum[i+1][j] + matrix[i][j] - sum[i][j];
-            }
+    vector<vector<int>>v;//create a vector for prefix sum 
+
+NumMatrix(vector<vector<int>>& mat) {
+    
+    int n=mat.size(),m=mat[0].size();//size of matrix n*m
+    
+    v.resize(n+1,vector<int>(m+1,0));//resize the prefix vector as size of matrix vector + 1 in row and column for handling base cases 
+    //Also mark all elements as zero
+    
+    //store the prefix sum in vector v
+    for(int i=1;i<=n;i++){ 
+        for(int j=1;j<=m;j++){
+            v[i][j]=v[i-1][j]+v[i][j-1]+mat[i-1][j-1]-v[i-1][j-1];
         }
     }
-    // TC O(1)
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return sum[row2+1][col2+1] - sum[row2+1][col1] - sum[row1][col2+1] + sum[row1][col1];
-    }
+}
+
+//TC:O(1)
+int sumRegion(int row1, int col1, int row2, int col2) {
+    //Return the answer as given below
+    return v[row2+1][col2+1]-v[row2+1][col1]-v[row1][col2+1]+v[row1][col1];
+}
 };
 
 /**
