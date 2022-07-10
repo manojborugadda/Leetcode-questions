537e3976cc64ad21db496b1ed581ca57360dc680
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-07-11 00:01:47 +0530

    Time: 7 ms (81.65%), Space: 13.9 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -3,8 +3,8 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
        vector<int>dp(n+1,0);
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        dp[0] = cost[0];// minimum cost to reach the step 0 is cost[0] only
+        dp[1] = cost[1];//minimum cost to reach the step 1 is taken as cost[1] as we can start from 0 index or 1 index as per the problem
         for(int i = 2;i<=n;i++){
             if(i==n){
                 return dp[i] = min(dp[i-1],dp[i-2]);
@@ -16,3 +16,5 @@ public:
         
     }
 };
+
+//first of all we have to find the Minimum cost to pay to reach the ith Step. for that we take the DP array
