132de9ac6dbcabcf0910b4ab01cb7af9bd2bfd2c
Borugadda Manoj <borugadda.manoj@gmail.com>
2022-07-10 23:38:58 +0530

    Time: 15 ms (27.88%), Space: 13.8 MB (28.79%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {  //TC:O(N)    SC:O(N)
+public: 
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+       vector<int>dp(n+1,0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2;i<=n;i++){
+            if(i==n){
+                return dp[i] = min(dp[i-1],dp[i-2]);
+            }
+            dp[i] = cost[i]+min(dp[i-1],dp[i-2]);
+        }
+        
+        return dp[n];
+        
+    }
+};
