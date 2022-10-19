8b04cf8625084fdd04a7e476be72bc387e1ccf3d
Manoj Borugadda <borugadda.manoj@gmail.com>
2022-10-20 02:04:11 +0530

    Time: 40 ms (8.16%), Space: 12.6 MB (56.36%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,24 @@
+class Solution { //TC:O(NLOGN)  SC:O(N)
+public:
+    //Custom comparator function 
+    struct comparator {
+    bool operator()(const pair<string,int> p1 , const pair<string,int> p2) {
+        return (p1.second == p2.second) ? (p1.first > p2.first) : (p1.second  < p2.second);
+    }
+};
+    
+    vector<string> topKFrequent(vector<string>& words, int k) {
+          unordered_map<string,int>map;
+          for(auto x:words) {
+            map[x]++;
+        }
+priority_queue<pair<string,int>, vector<pair<string,int>>, comparator> pq(map.begin(),map.end()); 
+        vector<string>ans;
+        while( pq.empty() == false and ans.size() < k ) {
+            string temp =  pq.top().first;
+            ans.push_back(temp);
+            pq.pop();
+        }
+        return ans;
+    }
+};
