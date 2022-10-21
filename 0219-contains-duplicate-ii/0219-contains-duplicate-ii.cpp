ddcadba56f27c9531bfb2bb035072085fe62d087
Manoj Borugadda <borugadda.manoj@gmail.com>
2022-10-21 14:31:22 +0530

    Time: 395 ms (42.33%), Space: 77.2 MB (60.05%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {  //TC:O(N)  SC:O(N)
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>map;
+        for(int i = 0;i<nums.size();i++) {
+            //if we find the duplicate of current number in the MAP we validate "abs(i - j) <= k."
+            if(map.find(nums[i]) != map.end() and abs(map[nums[i]] - i) <= k) {
+                return true;
+            }
+            //else
+            map[nums[i]] = i;  //insert into the HASHMAP element as KEY and index as VALUE
+        }
+        return false;
+    }
+};
