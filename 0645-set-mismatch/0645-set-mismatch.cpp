9943a84f365ecc955afe1508b977783b19595cd8
Manoj Borugadda <borugadda.manoj@gmail.com>
2022-10-23 21:22:37 +0530

    Time: 146 ms (22.29%), Space: 30.7 MB (34.35%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,22 @@
+class Solution { //TC:O(N)  SC:O(N)  //we are using extra space
+public://               using HASHMAPS
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        unordered_map<int,int>map;
+        //checking if any element is repeated  and if YES we push that into the ans vector
+        for(int i:nums) {
+            if(map.find(i)!=map.end()) ans.push_back(i);
+            map[i]++;
+        }
+        
+    //checking for the missing element using HASHMAP and if we got that we pushed into the vector
+        for(int i = 1;i<=n;i++) {
+            if(map.find(i)==map.end()) {
+                ans.push_back(i);
+                break;
+            }
+        }
+        return ans;
+    }
+};
