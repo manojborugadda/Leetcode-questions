999823ca9f6dc975823121d3899580c93bd6440a
Manoj Borugadda <borugadda.manoj@gmail.com>
2022-11-10 21:14:20 +0530

    Time: 42 ms (72.29%), Space: 133.2 MB (5.23%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -5,27 +5,28 @@ class Solution {  // TC:O(N) SC:O(N)
 public:
     string removeDuplicates(string s, int k) {
         
-        string ans = ""; // storing the answer string
+        string ans = "";              // storing the answer string
         vector<pair<char,int>>count; // pair which takes the "char" and its "frequency"
         
-        for(int i = 0;i<s.length();i++)
-        {   
+        for(int i = 0;i<s.length();i++) {   
             // if vector is empty OR top element of the vector IS NOT Equal to present element then PUSH into the vector
-            if(count.size()==0 or s[i] != count.back().first){
+            if(count.size() == 0 or s[i] != count.back().first) {
                 count.push_back({s[i],1});
+            }else {
+                count.back().second++;
             }
-            // if the top element freqency is EQUAL to the K we POP that element from the vector
-            else if(++count.back().second == k){
+            
+            // if the top element freqency is EQUAL to the K we POP that element from the vector        
+            if(count.back().second == k) { 
                 count.pop_back();
             }
         }
         
         int n = count.size();
         // forming the string from the elements present in the vector
-        for(int i = 0;i<count.size();i++){
+        for(int i = 0;i<count.size();i++) {
             ans  = ans+string(count[i].second,count[i].first);
         }
-        
         return ans;
     }
 };
