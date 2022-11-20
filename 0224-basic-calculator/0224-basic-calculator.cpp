c1d5124a99988bb0fe78ae9d4bc35d11e1a45ced
Manoj Borugadda <borugadda.manoj@gmail.com>
2022-11-20 23:46:57 +0530

    Time: 27 ms (40.30%), Space: 8.2 MB (43.51%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int res = 0 , sign = 1;
+        stack<int>st;
+        for(int i = 0;i<s.size();i++) {
+            char ch = s[i];
+            if(ch >= '0' and ch <= '9') {
+                int num = 0;
+                while(i < s.length() and s[i] >= '0' and s[i] <= '9')  {
+                    num = num*10 + (s[i++] - '0');
+                }
+                res += sign*num;
+                i--;
+            } else if(ch == '+') sign = 1;
+              else if(ch == '-') sign = -1;
+              else if( ch == '(' ) {
+                  st.push(res);
+                  st.push(sign);
+                  res = 0;
+                  sign = 1;
+              } else if( ch == ')' ) {
+                  res  = res * st.top();
+                  st.pop();
+                  res += st.top();
+                  st.pop();
+              }
+        }
+        return res;
+    }
+};
