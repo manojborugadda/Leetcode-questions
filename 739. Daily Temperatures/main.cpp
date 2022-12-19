67d5fc15ee4e457ecb6b8342cb69cb79ca4315e4
Manoj Borugadda <borugadda.manoj@gmail.com>
2022-12-20 02:23:46 +0530

    Update main.cpp

--- 739. Daily Temperatures/main.cpp
+++ 739. Daily Temperatures/main.cpp
@@ -1,1 +1,21 @@
+class Solution { //TC:O(N)  SC:O(N)
+public:
+    vector<int> dailyTemperatures(vector<int>& tp) {
+        
+        int i,j,n=tp.size();
+        vector<int> ans(n,0);
+        stack<int> s;
+
+        for(i=0;i<n;i++) {
+            while(!s.empty() &&  tp[s.top()] < tp[i]) {
+                ans[s.top()] = i - s.top();
+                s.pop();
+            }
+            s.push(i);
+        }
+        
+        return ans;
+    }
+};
+
 
