7964c45537d8e081546965dcd434b7588fc7ccf0
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-07-16 18:57:20 +0530

    Create main.cpp

---
+++ 2778. Sum of Squares of Special Elements/main.cpp
@@ -0,0 +1,13 @@
+class Solution {///TC:O(1)  SC:O(N)
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0;i<n;i++) {
+            if(n % (i+1) == 0) {
+                ans = ans+ nums[i]*nums[i];
+            }
+        }
+        return ans;
+    }
+};
