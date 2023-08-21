d238abd7af977663fd3b579978f908a066d25f32
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-08-22 02:12:02 +0530

    Update 459-repeated-substring-pattern.cpp

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -5,3 +5,28 @@ public:
         return ans.find(s) != -1; // it checks  first index of the substring is there or NOT// if it is not there means we return false;
     }
 };
+
+
+////////////////////
+
+class Solution {//TC : 2*sqrt(n)*O(2n)
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        for(int i = n/2;i>=1;i--) {
+            if(n%i == 0) {
+                int times = n/i;
+                string pattern = s.substr(0,i);
+                string newSubstring = "";
+                while(times--) {
+                    newSubstring += pattern;
+                }
+
+                if(newSubstring == s) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
