3bf993b572c09495608666ffcaf2e292f1cf06b9
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-09-08 23:59:25 +0530

    Update 0219-contains-duplicate-ii.cpp

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {  //TC:O(N)  SC:O(N)
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int>map;
+        unordered_map<int,int>map; 
         for(int i = 0;i<nums.size();i++) {
             //if we find the duplicate of current number in the MAP we validate "abs(i - j) <= k."
             if(map.find(nums[i]) != map.end() and abs(map[nums[i]] - i) <= k) {
