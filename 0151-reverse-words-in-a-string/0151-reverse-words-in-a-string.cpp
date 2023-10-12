e3fd9c835d6e5000c571697638af622c5b14676e
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-10-12 23:59:07 +0530

    Update 0151-reverse-words-in-a-string.cpp

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,7 +3,7 @@ public://TC:O(N)  SC:O(1)
     string reverseWords(string s) {
         stringstream ss(s);
         string ans,word;
-        while(ss>>word) {
+        while(ss>>word) { 
             ans =  word+" "+ans;
         }
         ans.pop_back();
