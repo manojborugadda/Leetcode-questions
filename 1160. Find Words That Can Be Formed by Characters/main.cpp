0fa4763f56b85133cd98cc673c638bfc952e38d9
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-12-03 01:00:07 +0530

    Update main.cpp

--- 1160. Find Words That Can Be Formed by Characters/main.cpp
+++ 1160. Find Words That Can Be Formed by Characters/main.cpp
@@ -1,1 +1,30 @@
 
+class Solution {//TC:O(N^2)  SC:O(N)
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        int ans = 0;
+        unordered_map<char,int>map;
+        for(auto ch : chars) {
+            map[ch]++;
+        }
+
+        for(int i = 0;i<words.size();i++) {
+            unordered_map<char,int>tempMap;
+            string temp = words[i];
+            for(auto ch : temp) {
+                tempMap[ch]++;
+            }
+            bool flag = true;
+            for(int k = 0;k<temp.length();k++) {
+                if(tempMap[temp[k]] > map[temp[k]]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                ans += temp.length();
+            }
+        }
+        return ans;
+    }
+};
