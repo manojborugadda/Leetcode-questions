6d69fa5ed2d27475180908f39674b9015bea279a
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-12-08 16:42:55 +0530

    Create another solution.cpp

---
+++ 606-construct-string-from-binary-tree/another solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void constructString(TreeNode* root , string &s) {
+        if(root == NULL) return ;
+        s += to_string(root->val);
+        if(root->left or root->right) {
+            s += "(" ;
+            constructString(root->left,s) ;
+            s+= ")";
+        }
+        if(root->right) {
+             s += "(" ;
+            constructString(root->right,s) ;
+            s+= ")";
+        }
+    }
+    string tree2str(TreeNode* root) {
+        string str = "";
+        constructString(root,str);
+        return str;
+    }
+};
