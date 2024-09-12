4a63b487aee95fc10b1b25228f1d5ada1a85bd01
Manoj Borugadda <borugadda.manoj@gmail.com>
2024-09-12 23:45:45 +0530

    Update 1684-count-the-number-of-consistent-strings.cpp

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-     unordered_map<char,int>mp;
-        for(auto i:allowed){
-           mp[i] ++;
-        }
-        int ans=0;
-        for(auto x:words){
-            bool isconsistent = true;
-            for(auto y:x){
-                if(mp[y]==0){
-                    isconsistent = false;
-                    break;
+        unordered_set<char>st(allowed.begin(),allowed.end());
+        int cnt=0;
+        for(int i=0;i<words.size();i++){
+            string temp=words[i];
+            bool ispresent=true;
+            for(int j=0;j<temp.length();j++){
+                if(st.find(temp[j])==st.end()){
+                    ispresent=false;break;
                 }
             }
-            
-            if(isconsistent==true)
-                ans++;
+            if(ispresent)cnt++;
         }
+        return cnt;
         
-        return ans;
     }
 };
+/**
+Time complexity: O(n∗m) where n is the number of words, and m is the average length of each word.
+
+Space complexity: O(k) , where k is the length of the allowed string, since we are storing characters in a set.
+*/
