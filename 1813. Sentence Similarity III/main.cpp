f9fe491f6c0027679ed6b04d6feefb06d75e5a9c
Manoj Borugadda <borugadda.manoj@gmail.com>
2024-10-07 00:56:54 +0530

    Update main.cpp

--- 1813. Sentence Similarity III/main.cpp
+++ 1813. Sentence Similarity III/main.cpp
@@ -1,34 +1,31 @@
-class Solution {//TC:O(N+M)+O(min(n,m))  SC:O(n+m)
+class Solution {
 public:
     bool areSentencesSimilar(string sentence1, string sentence2) {
-    if(sentence1.length() > sentence2.length()) {//recursive call bcoz we need sentence1 to be smaller
-        return areSentencesSimilar(sentence2,sentence1);
-    }
-    vector<string>v1,v2;
-    // Used for breaking words
-    stringstream ss(sentence1);
-    stringstream ss1(sentence1);
-    // To store individual words
-    string Word;
-    while (ss >> Word) {
-       v1.push_back(Word);
-    }
-    while (ss1 >> Word) {
-       v2.push_back(Word);
-    }
-
-
-    int start = 0 , end1 = v1.size()-1 , end2 = v2.size()-1;
-    //finding prefix words
-    while(start <= end1 and sentence1[start] == sentence2[start]) {
-        start++;
-    }
-    //finding suffix words
-    while(start <= end1 and sentence1[end1] == sentence2[end2]) {
-        end1--;
-        end2--;
-    }
-    return start > end1; //if we traverse all the small string elements beyond end1 it means we can make the strings equal
-
+        // recursive call bcoz we need sentence1 to be smaller
+        if(sentence1.length()>sentence2.length()){
+            return areSentencesSimilar(sentence2,sentence1);
+        }
+        vector<string>smaller,larger;
+        stringstream ss1(sentence1),ss2(sentence2);
+        string word;
+        // Split sentence1 into words and store in smaller vector.
+        while(ss1>>word){
+            smaller.push_back(word);
+        }
+        // Split sentence2 into words and store in larger vector.
+        while(ss2>>word){
+            larger.push_back(word);
+        }
+        int start=0;
+        int end1=smaller.size()-1;
+        int end2=larger.size()-1;
+        while(start<=end1 && smaller[start]==larger[start]){
+            start++;
+        }
+        while(start<=end1 && smaller[end1]==larger[end2]){
+            end1--;
+            end2--;
+        }
+        return start>end1;
     }
 };
