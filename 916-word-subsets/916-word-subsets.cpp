66e1aa6b633b8292c49f06430a1c57675378c359
Manoj Borugadda <borugadda.manoj@gmail.com>
2025-01-11 01:32:26 +0530

    Update 916-word-subsets.cpp

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,49 +1,38 @@
-//Time Complexity for this soltuion is : O(M + N) where m and n is size of words1 and words2 array
-//SC:O(WORDS1.SIZE())
-class Solution {
+class Solution {//SC:O(m+N)
 public:
-    // calculate the frequency of string s
-    vector<int> giveMeFreq(string s)
-    {
-        vector<int> freq(26,0);
-        for(int i = 0; i < s.length(); i++)
-        {
-            freq[s[i] - 'a']++;
+    bool isUniversal(string s1, unordered_map<char, int>& map) {
+        unordered_map<char, int> map1;
+        for (auto s : s1) {
+            map1[s]++;
         }
-        return freq;
+        for (auto [a, b] : map) {
+            if (map1[a] < b) {
+                return false;
+            }
+            if (map1.find(a) == map1.end()) {
+                return false;
+            }
+        }
+        return true;
     }
-    
-    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) 
-    {
-       vector<string> ans; // store ans
-       vector<int> max_Freq_w2(26, 0);   // store max freq of each character present in word2 stirngs
-	   
-        // we will Iterate over word to and try to find max freq for each character present in all strings.
-		for(auto &x : words2) 
-        {
-            vector<int> freq = giveMeFreq(x);
-            for(int i = 0; i < 26; i++)
-            {
-                max_Freq_w2[i] = max(freq[i], max_Freq_w2[i]);  // upadate freq to max freq
+
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        unordered_map<char, int> map;
+        for (auto word : words2) {//(m*n)
+            unordered_map<char, int> tempMap;
+            for (char ch : word) {
+                tempMap[ch]++;
+            }
+            for (auto [ch, freq] : tempMap) {
+                map[ch] = max(map[ch], freq);
             }
         }
-        
-		// we will iterate for each string in words1 ans if it have all charaters present in freq array with freq >= that     then we will add it to ans
-        for(auto &x : words1)
-        {
-            vector<int> freq = giveMeFreq(x);  // gives freq of characters for word in words1
-            bool flag = true;
-            for(int i = 0; i < 26; i++)
-            {
-                if(freq[i] < max_Freq_w2[i]) // specifies that word did not have all the characters from word2 array
-                {
-                    flag = false;
-                    break;
-                }
+        vector<string> ans;
+        for (auto word : words1) {//(L*M) -- L is the size  of the word1 vector
+            if (isUniversal(word, map)) {
+                ans.push_back(word);
             }
-            if(flag) ans.push_back(x);   // string x is Universal string
         }
         return ans;
     }
 };
-
