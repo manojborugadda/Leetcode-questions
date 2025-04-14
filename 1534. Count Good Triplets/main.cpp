c5375ffd1d25e2732ce29037baf2e8bb444db1c8
Manoj Borugadda <borugadda.manoj@gmail.com>
2025-04-15 02:29:40 +0530

    Create main.cpp

---
+++ 1534. Count Good Triplets/main.cpp
@@ -0,0 +1,18 @@
+// 1534. Count Good Triplets
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int cnt = 0;
+        int n = arr.size();
+        for(int i = 0; i < n - 2; i++) {
+            for(int j = i + 1; j < n - 1; j++) {
+                if(abs(arr[i] - arr[j]) > a) continue; // prune here
+                for(int k = j + 1; k < n; k++) {
+                    if(abs(arr[j] - arr[k]) > b) continue; // prune here
+                    if(abs(arr[i] - arr[k]) <= c) cnt++; // final check
+                }
+            }
+        }
+        return cnt;
+    }
+};
