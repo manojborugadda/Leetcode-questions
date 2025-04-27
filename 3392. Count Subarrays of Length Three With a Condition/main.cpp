5a354e20edcda658f6f9e5eaab1f17a1d5fe9fa8
Manoj Borugadda <borugadda.manoj@gmail.com>
2025-04-27 20:43:37 +0530

    Create main.cpp

---
+++ 3392. Count Subarrays of Length Three With a Condition/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        if(nums.size() < 3) return 0;
+        for(int i = 1;i<n-1;i++) {
+            if(nums[i] == 2* ( nums[i-1] + nums[i+1])) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
